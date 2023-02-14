9c31c66cbbc00aebc855350a41047dca086f6543
bangdler <zbthz90@gmail.com>
2023-02-14 18:01:47 +0900

    webpack dev & prod 분리, babel.config.json & tsconfig.json 분리, 기타 플러그인 추가설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import "./app.css"
+import "@/app.css"
 
 function App() {
     return (<>
---
+++ webpack.base.conf.js
@@ -0,0 +1,72 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const dotenv = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const aliases =require('./aliases')
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? '.env' : '.env.development'),
+});
+
+module.exports = {
+  name: 'react-typescript', // 웹팩 설정 이름
+
+  // 애플리케이션 시작 경로
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+
+  // 번들된 파일 경로
+  output: {
+    path: path.join(__dirname, 'dist'), //빌드 위치
+    filename: 'app.js', //웹팩 빌드 후 최종적으로 만들어질 파일
+    publicPath: '/',
+  },
+
+  resolve: {
+    alias: aliases.webpack,
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.svg$/i,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(), // 웹팩 실행시마다 dist 폴더 정리
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+    new MiniCssExtractPlugin(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'public/meta' }, // meta 폴더를 dist 에 복사
+    //    ],
+    // }),
+  ],
+};
